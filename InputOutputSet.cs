4976ccabecd0d81cf0fc0b87175ac1b0b71d67e4
Booperboberino <ngrosh@uncc.edu>
2023-09-05 10:57:46 -0400

    inputoutputset

---
+++ InputOutputSet.cs
@@ -0,0 +1,12 @@
+class InputOutputSet
+{
+    List<Neuron> input;
+    List<Neuron> expectedOutput;
+
+    public InputOutputSet(List<Neuron> input, List<Neuron> expectedOutput)
+    {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
+}
